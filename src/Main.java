74764e8c873c536d9a01c100d60349d2505a6ad6
yuliya_litvinova <yuliyalitvinovawork@gmail.com>
2023-01-13 13:53:01 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,19 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
+        int a = 345000;
+        byte b = -3;
+        short c = 770;
+        long  d = 9000000000000L;
+        float  e = 23.123456f;
+        double  f = 1074.12345678901234;
+        System.out.println("Значение переменной a с типом int = " +a);
+        System.out.println("Значение переменной b с типом byte = " +b);
+        System.out.println("Значение переменной c с типом short = " +c);
+        System.out.println("Значение переменной d с типом long = " +d);
+        System.out.println("Значение переменной e с типом float = " +e);
+        System.out.println("Значение переменной f с типом double = " +f);
+
 
 
     }
